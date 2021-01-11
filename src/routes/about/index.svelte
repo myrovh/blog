d5d0438b4128f035935eb3ed962f4f07976a18ba
myrovh <admin@myrovh.dev>
2021-01-11 21:38:52 +1100

    add styles to about

--- src/routes/about/index.svelte
+++ src/routes/about/index.svelte
@@ -2,11 +2,11 @@
   <title>Sapper project template</title>
 </svelte:head>
 
-<h1>YAY!</h1>
-
-<figure>
-  <img alt="siml" src="smile.png" />
-  <figcaption>HIGH FIVE!</figcaption>
-</figure>
-
-<p>The about me page</p>
+<article class="px-4 mx-auto prose dark:prose-light">
+  <h1>About</h1>
+  <p>It's empty right now.</p>
+  <figure>
+    <img alt="siml" src="smile.png" />
+    <figcaption>HIGH FIVE!</figcaption>
+  </figure>
+</article>
